2724ab0b675a4d646f41b92908f7f8ce40a36bc0
Keyan Pishdadian <kpishdadian@gmail.com>
2018-11-04 03:08:41 -0800

    Updates for route_planner

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,22 +6,38 @@ SCRIPT_DIR=$(pwd)
 
 # System packages
 sudo apt-get update
-sudo apt-get install --assume-yes nginx docker docker-compose dnsutils python-minimal python-pip
+sudo apt-get install --assume-yes nginx docker docker-compose dnsutils python-minimal python-pip g++ make
 
 # Personal configuration
 cd ~
 if [ ! -d ~/dotfiles ]; then
     git clone https://github.com/keyan/dotfiles.git
+    pushd dotfiles/
+    ./install && popd
 fi
-pushd dotfiles/
-./install && popd
 
 # Pull any project repos and start the docker-compose applications.
 if [ ! -d ~/tonic-time ]; then
     git clone https://github.com/keyan/tonic-time.git
 fi
 
+if [ ! -d ~/route_planner ]; then
+    git clone https://github.com/keyan/route_planner.git
+
+    # Install and build
+    pushd route_planner
+    make install
+    make build
+
+    # Get map data
+    wget https://overpass-api.de/api/map?bbox=-122.4333,47.5077,-122.1667,47.6706
+    mv map data/seattle.osm
+    perl scripts/osm_stripper.pl data/seattle.osm data/seattle.clean.osm
+    popd
+fi
+
 cd $SCRIPT_DIR
+
 # Runs the containers in the background
 sudo docker-compose up --build -d
 
